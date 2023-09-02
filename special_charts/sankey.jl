8aaaaa00b980ec3bfaf28f2fab1da10396390598
Anup Joseph <anup20joseph@gmail.com>
2023-09-02 19:36:08 +0530

    Added sankey charts

---
+++ special_charts/sankey.jl
@@ -0,0 +1,27 @@
+using PlotlyJS
+using ColorSchemes
+using Colors
+
+src = [1, 1, 1, 1, 2, 2, 2, 3, 4, 5]
+dst = [6, 3, 7, 4, 3, 7, 4, 7, 8, 8]
+weights = [0.1, 0.3, 0.5, 0.5, 0.2, 2.8, 1, 0.45, 4.5, 3.3]
+node_colors = hex.(colors.sequential[:tofino10])
+node_colors = "#" .* node_colors
+fig = plot(sankey(
+        node=attr(
+            pad=15,
+            thickness=20,
+            line=attr(color="black", width=0.5),
+            label=["A1", "A2", "B1", "B2", "C1", "C2"],
+            color=node_colors
+        ),
+        link=attr(
+            source=src, # indices correspond to labels, eg A1, A2, A1, B1, ...
+            target=dst,
+            value=weights
+        )),
+    Layout(title_text="Basic Sankey Diagram", font_size=10)
+)
+print("Red")
+
+colors.viridis
